8f12c06fc9e9e6f99c4a08f1e4da060141285356
icy-beep <iceghostse@gmail.com>
2026-05-13 23:39:07 +0300

    feat: Импорты в init файлах

--- src/entities/__init__.py
+++ src/entities/__init__.py
@@ -1,2 +1,7 @@
 from .coordinate import Coordinate
 from .entity import Entity
+from .defense_module import DefenseModule
+from .projectile import Projectile
+from .hostile_entity import HostileEntity
+from .turrets import LaserTurret, BulletTurret, MortarTurret
+from .enemies import DroneWalker, GiantRoach, ScoutDrone
---
+++ src/systems/__init__.py
@@ -0,0 +1,2 @@
+from .resource_bank import ResourceBank
+from .wave_protocol import WaveProtocol, WaveConfig
